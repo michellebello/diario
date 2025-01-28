5503a38bc44482aca90a381a9de6a4b70b76e56f
michellebello <michellebelloveramendi@gmail.com>
2025-01-28 17:11:22 -0500

    added reusables for user view

---
+++ src/components/CuentaView.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SideBar from "./reusables/SideBar.jsx";
+import TopBar from "./reusables/TopBar.jsx";
+
+function CuentaView() {
+  return (
+    <div>
+      <TopBar></TopBar>
+      <SideBar></SideBar>
+    </div>
+  );
+}
+
+export default CuentaView;
---
+++ src/components/reusables/SideBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SideBar() {
+  return <h1>ok</h1>;
+}
+
+export default SideBar;
